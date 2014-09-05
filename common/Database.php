043a6b99c9d655c410843744cdb2fc60fc932941
SheriefBadran <sheriefbadran@Sheriefs-MacBook-Air.local>
2014-09-05 14:40:04 +0200

    Database connection works fine.

---
+++ common/Database.php
@@ -0,0 +1,26 @@
+<?php
+
+	class Database {
+
+		private $connection;
+
+		function __construct () {
+			
+			$this->connection = mysql_connect(DB_HOST_NAME, DB_USER_NAME, DB_PASSWORD);
+			$selectedDB = mysql_select_db(DB_NAME, $this->connection);
+			var_dump($selectedDB);
+
+			if ($this->connection) {
+				
+				echo 'mysql connected!';
+
+				if (!$selectedDB) {
+					
+					echo 'Could not select database!';
+				}
+			} else {
+
+				echo 'mysql did not connect!';
+			}
+		}
+	}
